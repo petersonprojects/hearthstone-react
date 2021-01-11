a423747836a52935d89b46bed5878798967a5a4c
Micah Peterson <micahpeterson@live.com>
2021-01-11 12:05:31 -0600

    setting up proxy for access token

--- src/actions/cardActions.js
+++ src/actions/cardActions.js
@@ -13,15 +13,30 @@ export const loadCards = () => {
 
     return async (dispatch) => {
 
+        var accessToken;
+        // first make a call to localhost:3000/api to receive an oauth token as a response
+        // need to make this async
+        await fetch('http://localhost:3000/api')
+        .then(res => res.json())
+        .then(data => {
+            console.log(data.aToken)
+            accessToken = data.aToken
+        })
+        .catch(err => console.log(err))
+    
+
+        // working! using proxy
+        // console.log(accessToken);
+
         try{
 
             // api calls
             let cards = [];
             let mult = 1;
 
-            for(let page = 1;page <= 70;page++)
+            for(let page = 1;page <= 71;page++)
             {
-                let response = await axios.get(`https://us.api.blizzard.com/hearthstone/cards?locale=en_US&page=${page}&access_token=USOls9dRJqmUcbnQpw215C5T78tvD9k7Yv`)
+                let response = await axios.get(`https://us.api.blizzard.com/hearthstone/cards?locale=en_US&page=${page}&access_token=${accessToken}`)
 
                 cards.push(...response.data.cards);
 
@@ -31,7 +46,6 @@ export const loadCards = () => {
                     mult = mult + 1;
                 }
 
-                console.log(cards)
             }
 
         }
